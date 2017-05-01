f3f0c3354e48a46647e1e074f5cf31c7545af12a
liorgreenb <lior.greenb@gmail.com>
2017-05-01 14:38:31 +0300

    * Main route now extracts documentation based on file param

--- src/server/app.js
+++ src/server/app.js
@@ -2,13 +2,15 @@ var express = require('express')
 var app = express();
 import documentExtractor from "./services/documentExtractor";
 
-app.get('/:fileName', function (req, res) {
-  documentExtractor(req.query.fileName, content => {
-    res.send(content)
+/**
+ * Get this document
+ */
+app.get('/', function (req, res) {
+  documentExtractor(req.query.file, data => {
+    res.send(data);
   });
-  
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
--- src/server/services/documentExtractor.js
+++ src/server/services/documentExtractor.js
@@ -1,4 +1,19 @@
-export default function documentExtractor(fileName, callback) {
-    fs = require('fs');
-    fs.readFile(fileName, "utf8", callback);
+import * as fs from "fs";
+
+const DOCUMENTATION_REGEXP = /\/\*\*[\s\S]*\*\//gm;
+
+/**
+ * Extracts JSDoc from files
+ * @param {String} filePath The file path to read
+ * @param {Function} callback Function to invoke on resolve
+ */
+export default function documentExtractor(filePath, callback) {
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+
+        let matches = DOCUMENTATION_REGEXP.exec(data);
+        callback(matches && matches[0]);
+    });
 }
