eaffba31de70efe46c0f299e13dd9c868bb75b9f
liorgreenb <lior.greenb@gmail.com>
2017-06-16 21:17:12 +0300

    Create synonime replacer service for doctrine

--- client/services/parseDocumentation.js
+++ client/services/parseDocumentation.js
@@ -1,13 +1,17 @@
 import doctrine  from "doctrine";
+import replaceSynonimes from "./synonimeService";
 
 export function parseDocumentation(libraryDocs) {
     let docs = {};
     for (let name in libraryDocs) {
         let doc = libraryDocs[name];
         let ast = doctrine.parse(doc, {unwrap: true, recoverable: true, sloppy: true});
-        let parsedDoc = getDoc(ast);
+        let parsedDoc = getDoc(ast)
+        parsedDoc = replaceSynonimes(parsedDoc);
+
         docs[name] = parsedDoc;
     }
+    
     return docs;
 }
 
