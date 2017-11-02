4e67f6c9a1636fa32e1349f921108682fa5d18b0
Noam Elboim <noam.elboim@noam.elboim-mbp>
2017-11-02 21:59:39 +0200

    Support passing object instead of just JSDoc. Closes #59

--- client/services/parseDocumentation.js
+++ client/services/parseDocumentation.js
@@ -1,21 +1,31 @@
-import doctrine  from "doctrine-standalone";
-import replaceSynonyms from "./synonymService";
+import _ from 'underscore';
+import doctrine  from 'doctrine-standalone';
+import replaceSynonyms from './synonymService';
 
+/**
+ * Parse the JSDoc and turn them into pretty object, if received a "final-form" object - we will just pass it
+ * @param {Object} libraryDocs mapping between component name 
+ */
 export function parseDocumentation(libraryDocs) {
     let docs = {};
-    for (let name in libraryDocs) {
+    const componentsNames = _.keys(libraryDocs);
+    for (let i = 0, l = componentsNames.length; i < l; i++) {
+        const name = componentsNames[i];
         let doc = libraryDocs[name];
-        let ast = doctrine.parse(doc, {unwrap: true, recoverable: true, sloppy: true});
-        let parsedDoc = getDoc(ast)
-        parsedDoc = replaceSynonyms(parsedDoc);
-
+        let parsedDoc;
+        if (typeof doc === 'string') {
+            let ast = doctrine.parse(doc, {unwrap: true, recoverable: true, sloppy: true});
+            parsedDoc = replaceSynonyms(prettifyAst(ast));
+        } else {
+            parsedDoc = doc; // already an object
+        }
         docs[name] = parsedDoc;
     }
     
     return docs;
 }
 
-function getDoc({tags}) {
+function prettifyAst({tags}) {
     let doc = {};
     for (let i in tags) {
         let tag = tags[i];
